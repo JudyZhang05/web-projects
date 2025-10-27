8134e671c7af4f8815b2522a6ae1345d8e01fce6
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2025-10-27 16:50:13 -0400

    class 7 demo

---
+++ class-demos/week_07/main.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+
+    // adding 3 paragraph elements that will contain the sizes of the screen
+    let p = document.createElement('p')
+    let p2 = document.createElement('p')
+    let p3 = document.createElement('p')
+
+    document.getElementById('content').appendChild(p)
+    document.getElementById('content').appendChild(p2)
+    document.getElementById('content').appendChild(p3)
+
+    // adding event listener based on window when the size changes
+    window.addEventListener('resize', () => {
+
+        p.innerHTML = 'window inner width ' + window.innerWidth
+ 
+        p2.innerHTML = 'body inner width ' + document.getElementById('body').clientWidth
+ 
+        p3.innerHTML = 'screen width ' + screen.width
+        
+    })
+
+}

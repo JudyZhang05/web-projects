e39ed844ce6841373035b6dd6a80adb2a78b0243
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2025-11-03 16:53:59 -0500

    class 8 demo

---
+++ class-demos/week_08/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import {ref, onMounted } from "vue"
+
+  const myVariable = ref('hi')
+
+  const greetings = () => {
+    console.log('hi')
+  }
+
+  onMounted ( () => {
+    console.log(document.getElementById('blue-paragraph'))
+  })
+
+</script>
+
+<template>
+  <button @click="greetings"> {{myVariable}} </button>
+  <h1>You did it!</h1>
+  <p id='blue-paragraph'>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <p>
+    now i have some text in here
+  </p>
+</template>
+
+<style scoped></style>

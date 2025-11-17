f9455fe94917c3517a0e8a446c7ee910fec5c84e
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2025-11-17 16:19:55 -0500

    updates project 4

--- project4/vue-project/vite.config.js
+++ project4/vue-project/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "https://judyzhang.xyz/project4/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
